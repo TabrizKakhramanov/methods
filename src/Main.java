2fa57d0ec87b6d046c3a50be3198d506d81eff8a
TabrizKakhramanov <tabriz9998@gmil.com>
2024-10-27 15:29:11 +0300

    Made homework

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,14 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
         //task1 test
-        int year = 2021;
+        int year = 2022;
         int year2 = 2024;
-        System.out.println(isLeapYear(year));
-        System.out.println(isLeapYear(year2));
+        isLeapYear(year);
+        isLeapYear(year2);
         int clientOS = 0;
         int clientOS2 = 1;
-        chekDeviceVersion(year,clientOS);
-        chekDeviceVersion(year2,clientOS2);
+        checkDeviceVersion(year,clientOS);
+        checkDeviceVersion(year2,clientOS2);
         int deliveryDistance = 115;
         int deliveryDistance2 = 45;
         System.out.println(countDeliveryTime(deliveryDistance));
@@ -21,16 +21,17 @@ public class Main {
 
     }
     //task1
-    public static boolean isLeapYear (int year){
-        boolean result=false;
-        if ((year%4==0||year%400==0)&&(year%100!=0)){
-            result = true;
+    public static void isLeapYear (int year){
+        boolean isYearLeap = (((year-1584)%4==0)||((year-1584)%400==0))&&((year-1584)%100!=0);
+        if (isYearLeap){
+            System.out.println(year + " год является високосным.");
+        } else {
+            System.out.println(year + " год не является високосным.");
         }
-        return result;
     }
 
     //task2
-    public static void chekDeviceVersion (int deviceYear, int clientOS) {
+    public static void checkDeviceVersion (int deviceYear, int clientOS) {
         if ((deviceYear < LocalDate.now().getYear())&&clientOS == 1) System.out.println("«Установите облегченную версию приложения для Android по ссылке»");
         else if ((deviceYear == LocalDate.now().getYear())&&clientOS == 1) System.out.println("«Установите версию приложения для Android по ссылке»");
         else if ((deviceYear < LocalDate.now().getYear())&&clientOS == 0) System.out.println("«Установите облегченную версию приложения для iOS по ссылке»");
